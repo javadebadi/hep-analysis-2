74cb0349fd54c4087c1e3d4685cd50963dec25a9
javadebadi <javad.ebadi.1990@gmail.com>
2022-07-12 09:04:49 +0430

    Added an empty load module

---
+++ src/load/main.py
@@ -0,0 +1,5 @@
+"""Main function of load module
+"""
+
+def main():
+    pass
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 """
 import extract
 import transform
+import load
 
 MIN_CONTROL_NUMBER = 1
 MAX_CONTROL_NUMBER = 20000
@@ -10,6 +11,7 @@ SIZE = 1000
 def main():
     extract.main()
     transform.main()
+    load.main()
 
 
 if __name__ == '__main__':
